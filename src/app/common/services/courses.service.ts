f30cc672377d676fbc9c47480a327577c2836e82
oliaboria <olgaboris94@gmail.com>
2017-03-20 10:41:34 +0300

    cteate Course service method implementation

--- src/app/common/services/courses.service.ts
+++ src/app/common/services/courses.service.ts
@@ -1,12 +1,14 @@
-import { Injectable  } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Course } from '../interfaces';
 import { CourseItem } from '../helper-classes';
 
 @Injectable()
 export class CoursesService {
+	private courses: Course[] = [];
 
-	getCourses(): Course[] {
+	constructor() {
+		// This will be removed later. Add course is not implemented for now.
 		let description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. ' +
 					'Praesent libero. Sed cursus ante dapibus diam. ' +
 					'Sed nisi. Nulla quis sem at nibh elementum imperdiet. ' +
@@ -15,11 +17,17 @@ export class CoursesService {
 					'Class aptent taciti sociosqu ad litora torquent per ' +
 					'conubia nostra, per inceptos himenaeos.';
 
-		return [
-			new CourseItem('Video Course 1', new Date(), '1h 28min', description),
-			new CourseItem('Video Course 2', new Date(), '2h 15min', description),
-			new CourseItem('Video Course 3', new Date(), '45min', description),
-			new CourseItem('Video Course 4', new Date(), '1h 10min', description),
-		];
+		this.createCourse('Video Course 1', new Date(), '1h 28min', description);
+		this.createCourse('Video Course 2', new Date(), '2h 15min', description);
+		this.createCourse('Video Course 3', new Date(), '45min', description);
+		this.createCourse('Video Course 4', new Date(), '1h 10min', description);
+	}
+
+	createCourse(title: string, date: Date, duration: string, description: string): void {
+		this.courses.push(new CourseItem(title, date, duration, description));
+	}
+
+	getCourses(): Course[] {
+		return this.courses;
 	}
 }

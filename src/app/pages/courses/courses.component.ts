99188d63adfab6b96b60f928f52688382aa68278
oliaboria <olgaboris94@gmail.com>
2017-04-15 20:24:26 +0300

    unsubscribe from Observables

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component,
 		 ChangeDetectionStrategy,
+		 ChangeDetectorRef,
 		 OnInit,
 		 OnDestroy,
 		 ViewEncapsulation } from '@angular/core';
@@ -17,24 +18,33 @@ import { LoaderBlockService } from '../../common/components/loader-block';
 	templateUrl: './courses.template.html',
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent implements OnInit, OnDestroy {
 	items: Course[];
+	coursesSub: Subscription[] = [];
 
 	constructor(private coursesService: CoursesService, private loaderBlockService: LoaderBlockService) {
 		this.items = [];
 	}
 
 	ngOnInit(): void {
-		this.coursesService.getCourses().subscribe((items: Course[]) => {
-			this.items = items;
+		this.coursesSub.push(
+			this.coursesService.getCourses().subscribe((items: Course[]) => {
+				this.items = items;
+		}));
+	}
+
+	ngOnDestroy(): void {
+		this.coursesSub.forEach((sub: Subscription) => {
+			sub.unsubscribe();
 		});
 	}
 
 	onDelete(id: number): void {
-		this.coursesService.removeCourse(id).subscribe(() => {
-			setTimeout(() => {
-					this.loaderBlockService.hide();
-			}, 1000);
-		});
+		this.coursesSub.push(
+			this.coursesService.removeCourse(id).subscribe(() => {
+				setTimeout(() => {
+						this.loaderBlockService.hide();
+				}, 1000);
+		}));
 	}
 }
